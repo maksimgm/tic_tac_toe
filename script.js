fcd320c384f7b3713fe47253ee72f6fefc3b9f03
maksimgm <maksimmamrikov@gmail.com>
2015-08-16 17:49:12 -0700

    add all js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// window.onload{
-
-// }
-
+var playerX = {};
+var playerO = {};
 var turn = true;
 var letter = 'letter_x.jpeg';
 var oneBox = document.querySelectorAll(".row .box");
 addXsAndOs();
 reset();
+
+
 function changeTurn(){
   turn = !turn;
 }
@@ -15,8 +15,10 @@ function changeTurn(){
 
 function turnChanger(){
   if (turn) {
+    //playerO is global 
     letter = 'letter_o.jpeg';
   }else{
+    //playerX is global
     letter = 'letter_x.jpeg';
   }
 }
@@ -30,29 +32,28 @@ function turnChanger(){
 
 function addXsAndOs(){
   for (var i =0; i<oneBox.length; i++) {
+    oneBox[i].square = i;
     oneBox[i].addEventListener('click',function(){
-      // prevent override on existing clicks
       if (!this.move){
         this.querySelector('img').src = letter;
         this.move = letter;
+        //currentPlayer.indexString = true
+        if(turn){
+          playerX[this.square] = true;
+        }else{
+          playerO[this.square] = true;
+        }
+        // playerX[this.square] = true;
+        console.log(turn);
+        // playerO[this.square] 
         turnChanger();
         changeTurn();
+        winningCombination();
       }
     });
   }  
 }
 
-
-// determine winner
-
-// winning combinations [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]]
-//  for(var i = 0; i<arr.length; i++){
-//   for(var j = 0; j<arr[i].length; j++){
-//     console.log(arr[i][j]);
-//   }
-// }
-
-// RESETER
 
 function reset(){
   var button = document.getElementById('reset'); 
@@ -60,6 +61,24 @@ function reset(){
   for(var i =0; i<oneBox.length; i++){
     oneBox[i].querySelector('img').src = '';
     oneBox[i].move = '';
+    playerX = {};
+    playerO = {};
+    turn = true;
     }
   });
+}
+// determine winner
+
+ // winning combinations 
+ var arr = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+ 
+ function winningCombination(){
+  for(var i = 0; i<arr.length; i++){
+    if (playerX[arr[i][0]] && playerX[arr[i][1]] && playerX[arr[i][2]]){
+      alert("Player X Wins!!!");
+    }
+    if (playerO[arr[i][0]] && playerO[arr[i][1]] && playerO[arr[i][2]]){
+      alert("Player O Win!!!");
+    }
+  }
 }
